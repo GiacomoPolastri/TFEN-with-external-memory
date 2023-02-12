34422d6e882c86015a01acd1b64de6d9b3b995da
GiacomoPolastri <giacomo.polastri@GMAIL.COM>
2023-02-12 18:38:53 +0900

    update dataset

---
+++ Data/AMdataset.py
@@ -0,0 +1,71 @@
+
+
+import fiftyone as fo 
+
+dataset_type = fo.types.COCODetectionDataset
+dataset_dir = "./TFEN-with-external-memory/Inputs/Aerial_Maritime.v9-tiled.coco/"
+
+class LoaDataset():
+    
+    def __init__(self):
+        
+        self.trainset = self.loadTrainset()
+        self.validset = self.loadValidset()
+        self.testset = self.loadTestset()
+    
+    def loadTrainset(self):
+    
+        name = "Aerial_Maritime_trainset"
+        trainSet_dir = dataset_dir + "train"
+        label_path = trainSet_dir + "/_annotations.coco.json"
+        
+        traintest = fo.Dataset.from_dir(
+            data_path = trainSet_dir,
+            dataset_type = dataset_type,
+            labels_path = label_path,
+            name = name
+        )   
+        
+        return traintest
+    
+    def loadValidset(self):
+        
+        name = "Aerial_Maritime_validset"
+        validSet_dir = dataset_dir + "valid"
+        label_path = validSet_dir + "/_annotations.coco.json"
+
+        validtest = fo.Dataset.from_dir(
+            data_path = validSet_dir,
+            dataset_type = dataset_type,
+            labels_path = label_path,
+            name = name
+        )   
+
+        return validtest
+    
+    def loadTestset(self):
+        
+        name = "Aerial-Maritime-testset"
+        testSet_dir = dataset_dir + "test"
+        label_path = testSet_dir + "/_annotations.coco.json"
+
+        testset = fo.Dataset.from_dir(
+            data_path = testSet_dir,
+            dataset_type = dataset_type,
+            labels_path = label_path,
+            name = name
+        )   
+
+        return testset
+    
+if __name__ == "__main__":
+    
+    print (LoaDataset().trainset)
+    if fo.dataset_exists('Aerial_Maritime_trainset'):
+        print ('yes')
+        
+    validset = fo.load_dataset('Aerial_Maritime_validset')
+    print (validset)
+    
+    session = fo.launch_app(validset, desktop=True)
+    session.wait()
---
+++ Inputs/Aerial_Maritime.v9-tiled.coco/README.roboflow.txt
@@ -0,0 +1,15 @@
+
+Aerial Maritime - v9 tiled
+==============================
+
+This dataset was exported via roboflow.ai on August 9, 2020 at 4:20 AM GMT
+
+It includes 508 images.
+Movable-objects are annotated in COCO format.
+
+The following pre-processing was applied to each image:
+* Auto-orientation of pixel data (with EXIF-orientation stripping)
+
+No image augmentation techniques were applied.
+
+
